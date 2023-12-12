082bab329a103cda834e1755639d23fd9e12f2f4
Marco Roth <marco.roth@intergga.ch>
2023-12-12 23:51:27 +0100

    Add Repro for prefilled input action bug

--- app/avo/actions/release_fish.rb
+++ app/avo/actions/release_fish.rb
@@ -1,19 +1,7 @@
 class Avo::Actions::ReleaseFish < Avo::BaseAction
   self.name = "Release fish"
-  self.message = "Are you sure you want to release this fish?"
 
   def fields
-    field :message, as: :trix, help: "Tell the fish something before releasing."
-    field :user, as: :belongs_to, searchable: true, visible: -> {
-      resource.params[:id].present?
-    }
-  end
-
-  def handle(**args)
-    args[:records].each do |record|
-      record.release
-    end
-
-    succeed "#{args[:records].count} fish released with message '#{args[:fields][:message]}'."
+    field :name, help: "This field is populated from the `name` field form the model just because it has the same name, but I think it shouldn't"
   end
 end
